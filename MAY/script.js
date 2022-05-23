4d8e96cca3968bb73a2a3606c0e3606375a693b4
srushtidafe <100861585+srushtidafe@users.noreply.github.com>
2022-05-23 14:56:45 +0530

    completed May-Week3-test

---
+++ MAY/script.js
@@ -0,0 +1,38 @@
+const data = {
+  labels: [
+    'Red',
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [11, 16, 7, 3, 14],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
+  }]
+};
+// </block:setup>
+
+// <block:config:0>
+const config = {
+  type: 'polarArea',
+  data: data,
+  options: {}
+};
+// </block:config>
+
+module.exports = {
+  actions: [],
+  config: config,
+};
+
+$(document).ready(function () {
+    $('#example').DataTable();
+});
